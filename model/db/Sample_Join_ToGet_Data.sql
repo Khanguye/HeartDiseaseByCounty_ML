3533cce641afeff15d71d3b35a332bf4669e97e7
Ayyappa Muthusami <ayyappamuthusami@Ayyappas-MacBook-Pro.local>
2020-05-30 12:10:50 -0700

    added renaming of tables and columns based on feedback

--- model/db/Sample_Join_ToGet_Data.sql
+++ model/db/Sample_Join_ToGet_Data.sql
@@ -1,12 +1,12 @@
 SELECT 
-fc.*, hdmc.Rate_100000, hdmc.Rate_Level, psc.POP_ESTIMATE
+pofsc.*, hdmsc.Rate_100000, hdmsc.Rate_Level, psc.POP_ESTIMATE
 FROM 
-Heart_Disease_Mortality_County hdmc
-INNER JOIN State_symbol ss
-ON ss.Symbol = hdmc.State
-INNER JOIN (Features_County fc, Population_State_County psc)
-ON (fc.State = ss.StateName AND psc.State = ss.Symbol)
-WHERE hdmc.County = fc.County
-AND fc.County = psc.County;
+Heart_Disease_Mortality_State_Counties hdmsc
+INNER JOIN State s
+ON s.Code = hdmsc.StateCode
+INNER JOIN (Physical_Other_Features_State_County pofsc, Population_State_County psc)
+ON (pofsc.StateName = s.Name AND psc.StateCode = s.Code)
+WHERE hdmsc.County = pofsc.County
+AND pofsc.County = psc.County;
 
 
--- model/db/heart_disease_bycounty_ddl.sql
+++ model/db/heart_disease_bycounty_ddl.sql
@@ -1,26 +1,26 @@
-CREATE TABLE "State_symbol" (
-	"StateName"	TEXT,
-	"Symbol"	TEXT,
-	PRIMARY KEY("Symbol")
+CREATE TABLE "State" (
+	"Name"	TEXT,
+	"Code"	TEXT,
+	PRIMARY KEY("Code")
 );
 
-CREATE TABLE "Heart_Disease_Mortality_County" (
-	"State"	TEXT,
+CREATE TABLE "Heart_Disease_Mortality_State_Counties" (
+	"StateCode"	TEXT,
 	"County"	TEXT,
 	"Rate_100000"	NUMERIC,
 	"Rate_Level"	INTEGER,
-	PRIMARY KEY("State","County")
+	PRIMARY KEY("StateCode","County")
 );
 
 CREATE TABLE "Population_State_County" (
-	"State"	TEXT,
+	"StateCode"	TEXT,
 	"County"	TEXT,
 	"POP_ESTIMATE"	INTEGER,
-	PRIMARY KEY("State","County")
+	PRIMARY KEY("StateCode","County")
 );
 
-CREATE TABLE "Features_County" (
-	"State"	TEXT,
+CREATE TABLE "Physical_Other_Features_State_County" (
+	"StateName"	TEXT,
 	"County"	TEXT,
 	"Percent_Fair_or_Poor_Health"	INTEGER,
 	"Average_Number_of_Physically_Unhealthy_Days"	REAL,
@@ -51,5 +51,5 @@ CREATE TABLE "Features_County" (
 	"Percent_Adults_with_Diabetes"	INTEGER,
 	"Percent_Limited_Access_to_Healthy_Foods"	INTEGER,
 	"Median_Household_Income"	INTEGER,
-	PRIMARY KEY("State","County")
+	PRIMARY KEY("StateName","County")
 );
